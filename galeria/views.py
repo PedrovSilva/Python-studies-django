7677bf6a183270b5c04f4b1a198876dbfadc8862
Pedro Vitor <82241565+PedrovSilva@users.noreply.github.com>
2024-12-18 19:10:02 +0000

    search bar

--- galeria/views.py
+++ galeria/views.py
@@ -11,3 +11,13 @@ def index(request):
 def imagem(request, foto_id):
     foto = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, "galeria/imagem.html", {"foto": foto})
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET["buscar"]
+        if nome_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+
+    return render(request, "galeria/buscar.html", {"cards": fotografias})
